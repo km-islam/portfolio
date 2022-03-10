3794b4a32e2fb336cd994a504ea46b9345e19ecd
Kamrul Islame <kmislam.raju@gmail.com>
2022-03-10 10:03:26 +0600

    project initialised

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import About from "./component/About/About";
+import Contact from "./component/Contact/Contact";
+import Service from "./component/Service/Service";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <About />
+      <Service />
+      <Contact />
     </div>
   );
 }
